e5dad79beb24a80d32498afaeda933e95c97976a
DongjinHa <33052838+DongjinHa@users.noreply.github.com>
2020-03-25 17:22:47 +0900

    불필요소스 제거

--- src/main/java/com/msa/service/ReviewService.java
+++ src/main/java/com/msa/service/ReviewService.java
@@ -2,7 +2,6 @@ package com.msa.service;
 
 import java.util.List;
 
-import com.msa.document.Review;
 import com.msa.dto.CommentDTO;
 import com.msa.dto.ReviewDTO;
 import com.msa.dto.ReviewerDTO;
@@ -11,10 +10,7 @@ public interface ReviewService {
 
 	public List<ReviewDTO> getReviewList(ReviewDTO reviewDTO);
 	public List<ReviewDTO> getReviewList1();
-	public List<Review> getReviewList2();
-	public List<ReviewDTO> getReviewList3(String name);
     //public Optional<ReviewDTO> getReview(String id);
-	public List<ReviewDTO> getReviewList4(ReviewDTO reviewDTO);
 	public ReviewDTO getReview(String id);
     public void delReview(String id);
 	public ReviewerDTO getReviewer(String id);
